6f0f4e35397e624916fc15132319c4fc80e45413
J. Vitor <srvitorcastro@gmail.com>
2023-04-04 21:41:22 -0300

    Criação do contador de programa (PC)

---
+++ PC/PC.v
@@ -0,0 +1,15 @@
+module PC (clk, rst, count);
+
+	input clk, rst;
+	output reg [7:0] count;
+	
+   always @(posedge clk or posedge rst)
+   begin
+      if (rst)
+         count <= 8'b00000000;
+      else
+         count <= count + 1;
+   end
+
+endmodule
+
