88b339fdfa0e842c31fe6b45be8036afadac4539
Irlan Farias <80763521+IrlanAndrade@users.noreply.github.com>
2023-04-22 20:33:54 -0300

    mt odio

---
+++ i_mem.v
@@ -0,0 +1,21 @@
+module i_mem (
+  input 		 [31:0] address,
+  output reg [31:0] i_out
+);
+
+	reg [31:0] mem [0:1023];
+
+	initial begin
+	
+		$readmemb("instruction.list", mem);
+		
+	end
+
+	always @ (address) 
+	begin
+	
+		i_out <= mem[address]; // Lê a instrução na posição indicada por address 
+		
+	end
+
+endmodule
